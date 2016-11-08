843a07251ccd24cdd5defb14c1b454f4680764aa
Hasan Hameed <hasan.hameed@tenpearls.com>
2016-11-08 14:34:35 +0500

    first commit

---
+++ settings.class.ts
@@ -0,0 +1,7 @@
+export class SettingsConfig {
+    backgroundImage: true;
+    translation: 'ur.jalandhry';
+    showEnglish: true;
+    userLocation: 'Karachi, Pakistan';
+    userPrayerMethod: 1;
+};
