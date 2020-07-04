1632181fd191e24fc9943c19324cc430de4239fe
Saad <syed.saad@iomechs.com>
2020-07-04 15:00:08 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'chrome-storage-library';
+}
